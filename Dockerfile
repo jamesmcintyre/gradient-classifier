52271ba7cec9a2ba4aed9d9f460e1dad0d3c7611
James McIntyre <james@Jamess-MacBook-Pro.local>
2017-07-27 21:15:33 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Pull base image.
+FROM tensorflow/tensorflow:1.1.0
+
+# This docker image belongs to the community.
+# This docker image has (or will have) more than one "maintainer", comment your information
+
+MAINTAINER James McIntyre
+
+# Replace shell with bash so we can source files
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+
+# make sure apt is up to date
+RUN apt-get update --fix-missing
+RUN apt-get install -y curl
+RUN apt-get install -y build-essential libssl-dev
+
+ENV NVM_DIR /usr/local/nvm
+ENV NODE_VERSION 6.11.1
+
+# Install nvm with node and npm
+RUN curl https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash \
+    && source $NVM_DIR/nvm.sh \
+    && nvm install $NODE_VERSION \
+    && nvm alias default $NODE_VERSION \
+    && nvm use default
+
+ENV NODE_PATH $NVM_DIR/v$NODE_VERSION/lib/node_modules
+ENV PATH      $NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH
---
+++ src/server/tf-server/tf_image_classifier_server.py
@@ -0,0 +1,42 @@
+import os, sys
+
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line
+                   in tf.gfile.GFile("retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+
+
+def classifyImage():
+    # change this as you see fit
+    # image_path = sys.argv[1]
+    image_path = input("waiting for image path...")
+    # Read in the image_data
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+while True:
+    classifyImage()
